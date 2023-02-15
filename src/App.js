e6117a47b25d718b7bbb13bdb72bd0035dd16c64
Kanishk <kanishk.srivastava72@gmail.com>
2023-02-15 15:54:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,84 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Hello from './Test/Hello';
-import Summary from './Test/Summary';
 import { useState } from 'react';
+import data from './component/mock-data.json';
+import ReadOnlyRow from "./component/ReadOnlyRow";
 
 function App() {
 
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    fullName: "",
+    address: ""
+  });
 
-  const [inputList, setInputList] = useState([{
-    inputText: "",
-    positionType: "",
-    optionType: "",
-    ExpiryType: "",
-    selectStrikeCriteria: "",
-    simpleMomentum: "",
-    trailSL: "",
-    simpleMomentumValue: "",
-    trailSLvalue: "",
-    check_one: false,
-    check_two: false
-  }]);
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+    setAddFormData(newFormData);
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+      fullName: addFormData.fullName,
+      address: addFormData.address
+    };
+
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+
+    setAddFormData({
+      fullName: '',
+      address: ''
+    })
+  };
 
 
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Hello inputList={inputList} setInputList={setInputList} />}></Route>
-          <Route exact path="/SummaryPage" element={<Summary inputList={inputList} setInputList={setInputList} />}></Route>
-        </Routes>
-      </BrowserRouter>
+    <div className="app-container">
+      <h2>Add a Contact</h2>
+      <form onSubmit={handleAddFormSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder="Enter a name..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="address"
+          required="required"
+          placeholder="Enter an addres..."
+          onChange={handleAddFormChange}
+        />
+        <button type="submit">Add</button>
+      </form>
+
+      <form>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              <>
+                <ReadOnlyRow contact={contact} />
+              </>
+            ))}
+          </tbody>
+        </table>
+      </form>
     </div>
   );
 }
